d1b90e14c527e0e8a54149fa8ac0d139112d6591
Dylan Adams <113365542+max5max46@users.noreply.github.com>
2023-01-10 09:58:22 -0400

    Initial commit

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 0.5f;
+
+    public GameObject Camera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("w"))
+        {
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        }
+        if (Input.GetKey("s"))
+        {
+            transform.Translate(Vector3.back * speed * Time.deltaTime);
+        }
+        if (Input.GetKey("a"))
+        {
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+        }
+        if (Input.GetKey("d"))
+        {
+            transform.Translate(Vector3.right * speed * Time.deltaTime);
+        }
+
+    }
+}
